9d849831a1bfe95ebce4d5dd5a0fede5144b90c0
aikbix <ian@aikbix.com>
2012-10-27 16:38:32 -0700

    A more elegent notification system

--- src/simple_blog/views/posts.clj
+++ src/simple_blog/views/posts.clj
@@ -8,70 +8,6 @@
         [simple-blog.models.post]))
 
 ;==============================================================================
-;Pages
-;==============================================================================
-
-;------------------------------------------------------------------------------
-; Success/failure messages
-;
-;   User Login
-;   User Creation
-;------------------------------------------------------------------------------
-
-(defpage [:get "/user/new/failure"] []
-         (html5
-           [:p "Failed to create a new user."]))
-
-(defpage [:get "/user/new/success"] []
-         (html5
-           [:p "User created successfully."]))
-
-(defpage [:get "/user/login/success"] []
-         (html5
-           [:p "Login success."]))
-
-(defpage [:get "/user/login/failure"] []
-         (html5
-           [:p "Login failure."]))
-
-;------------------------------------------------------------------------------
-; Interactive pages (forms)
-; 
-;   User Login
-;   User Creation
-;------------------------------------------------------------------------------
-
-(defpage [:get "/user/login"] {:as user}
-         (userLoginForm user))
-
-(defpage [:get "/user/new"] {:as user}
-         (userAddForm user))
-
-
-;==============================================================================
-; POST functions
-;==============================================================================
-
-;------------------------------------------------------------------------------
-; Auth
-;
-;   User Login
-;   User Creation
-;------------------------------------------------------------------------------
-
-(defpage [:post "/user/login"] {:keys [username password]}
-         (login! username password)
-         (resp/redirect "/user/login/success"))
-
-(defpage [:post "/user/new"] {:keys [username password verify]}
-         (if (and (= verify password)
-                  (user-available? username))
-           (do
-            (add-user? username password)
-            (resp/redirect "/user/new/success"))
-           (resp/redirect "/user/new/failure")))
-
-;==============================================================================
 ; Form structures
 ;
 ;   User Login
@@ -90,7 +26,6 @@
             (userLogin [username password])
             (label "verify" "Verify Password: ")
             (text-field "verify" password))
-
 
 ;==============================================================================
 ; Partial forms
@@ -111,3 +46,54 @@
                      (userAdd user)
                      (submit-button "Create account")))
 
+;==============================================================================
+;Pages
+;==============================================================================
+
+;------------------------------------------------------------------------------
+; Success/failure messages
+;
+;   User Login
+;   User Creation
+;------------------------------------------------------------------------------
+
+(defn notify
+  [message]
+  (html5
+    [:p message]))
+
+;------------------------------------------------------------------------------
+; Interactive pages (forms)
+; 
+;   User Login
+;   User Creation
+;------------------------------------------------------------------------------
+
+(defpage [:get "/user/login"] {:as user}
+         (userLoginForm user))
+
+(defpage [:get "/user/new"] {:as user}
+         (userAddForm user))
+
+;==============================================================================
+; POST functions
+;==============================================================================
+
+;------------------------------------------------------------------------------
+; Auth
+;
+;   User Login
+;   User Creation
+;------------------------------------------------------------------------------
+
+(defpage [:post "/user/login"] {:keys [username password]}
+         (login! username password)
+         (notify "Login success."))
+
+(defpage [:post "/user/new"] {:keys [username password verify]}
+         (if (and (= verify password)
+                  (user-available? username))
+           (do
+            (add-user? username password)
+            (notify "User created successfully."))
+          (notify "Failed to create a new user.")))
