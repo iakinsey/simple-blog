f4a3d075537538cb0001243b08cfe66c972b97a5
aikbix <ian@aikbix.com>
2012-10-28 18:42:35 -0700

    Deprecated old dependencies

--- project.clj
+++ project.clj
@@ -2,7 +2,5 @@
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [noir "1.3.0-beta3"]
-                           [com.novemberain/monger "1.3.1"]
-                           [clj-time "0.4.4"]
-                           [cheshire "4.0.3"]]
+                           [com.novemberain/monger "1.3.1"]]
             :main simple-blog.server)
